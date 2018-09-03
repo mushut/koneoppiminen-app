8b6c851e3415a7633f4c0c6607a50ea2947fd83c
Aki Sipovaara <mushut@gmail.com>
2018-09-03 03:40:32 +0300

    Commenting.

--- build_dataset/build_dataset.py
+++ build_dataset/build_dataset.py
@@ -1,19 +1,27 @@
-# Program for building training, test and validation datasets
+# Program for building training, test and validation data sets
 # Aki Sipovaara 2018
 
 import csv
 
-
+# Read csv file
 def read_csv(filename):
     with open(filename, newline='') as csvfile:
         data_reader = csv.reader(csvfile, delimiter=';')
         titles = next(data_reader)
 
+        # Training data set
+
+        # Validation data set
+
+        # Test data set
+
+        # For testing purposes
         return titles
 
 
 def main():
-    print('Test')
+    print('Build data set')
+    # Tested printing titles of csv file
     print(read_csv('..\dataset\Online_Retail.csv'))
 
 
